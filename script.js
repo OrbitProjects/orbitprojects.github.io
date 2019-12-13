8234ac2b2033ec82cc76dd67ab80c880932b26ed
Ben Masel <masel.benjamin@gmail.com>
2019-12-13 11:06:19 +0800

    Added Effects

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
 window.sr = ScrollReveal();
 sr.reveal('.anim');
-
-var menu = document.getElementById("menubutton");
-menu.addEventListener("mouseover", function() {
-  menu.classList.add("fa-caret-down");
-  menu.classList.remove("fa-bars");
-});
-
-menu.addEventListener("mouseout", function() {
-  menu.classList.remove("fa-caret-down");
-  menu.classList.add("fa-bars");
-});
